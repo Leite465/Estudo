1d66ba4a0e7f063b4f7ee5e1e406b0aa3b285ca9
Gabriel Leite <gabriel-leite2014@outlook.com>
2017-10-02 17:14:44 -0300

    calculadora v1.1.1.4
    Comentei o código para facilitar o entendimento

--- calculadora/main.py
+++ calculadora/main.py
@@ -1,11 +1,12 @@
+#importando a biblioteca para operações matematicas
 import math
-
+#definindo alguns valores para 0
 res=0
 num1=0
 num2=0
 reps=0
 r=int(0)
-
+#menu de escolha simples
 print("1-Soma")
 print("2-subtração")
 print("3-multiplicação")
@@ -18,7 +19,7 @@ print("9-tangente")
 desc=int(input("Qual operação deseja realizar? "))
 
 while r==0:
-
+    #menu de escolha em caso de repetição
     if reps==1:
         print("1-Soma")
         print("2-subtração")
@@ -31,7 +32,7 @@ while r==0:
         print("9-tangente")
 
         desc=int(input("Qual operação deseja realizar? "))
-
+    #soma
     if desc==1:
         num1=int(input("qual é o primeiro número? "))
         num2=int(input("qual é o segundo número? "))
@@ -39,6 +40,7 @@ while r==0:
         res=num1+num2
 
         print("O resultado da operação é {}!".format(res))
+    #subtração
     elif desc==2:
         num1 = int(input("qual é o primeiro número? "))
         num2 = int(input("qual é o segundo número? "))
@@ -46,6 +48,7 @@ while r==0:
         res = num1 - num2
 
         print("O resultado da operação é {}!".format(res))
+    #multiplicação
     elif desc==3:
         num1 = int(input("qual é o primeiro número? "))
         num2 = int(input("qual é o segundo número? "))
@@ -53,6 +56,7 @@ while r==0:
         res = num1 * num2
 
         print("O resultado da operação é {}!".format(res))
+    #divisão
     elif desc==4:
         num1 = int(input("qual é o primeiro número? "))
         num2 = int(input("qual é o segundo número? "))
@@ -60,12 +64,14 @@ while r==0:
         res = num1 / num2
 
         print("O resultado da operação é {}!".format(res))
+    #raiz quadrada com a função sqrt da biblioteca math
     elif desc==5:
         num1 = int(input("qual é o número? "))
 
         res = math.sqrt(num1)
 
         print("O resutado da operação é {}!".format(res))
+    #potenciação
     elif desc==6:
         num1 = int(input("qual é o número a ser elevado? "))
         num2 = int(input("qual é o número a se elevar? "))
@@ -73,13 +79,15 @@ while r==0:
         res = math.pow(num1, num2)
 
         print("O resultado da operação é {}!".format(res))
+    #seno
     elif desc==7:
         num1 = int(input("qual é o angulo que desega encontrar o seno? "))
 
-        res=math.radians(num1)
-        res=math.sin(res)
+        res=math.radians(num1)#converto o angulo para radianos
+        res=math.sin(res)#calculo o seno
 
         print("O seno de {} é {}!".format(num1, res))
+    #coseno
     elif desc==8:
         num1 = int(input("qual é o angulo que desega encontrar o coseno? "))
 
@@ -87,6 +95,7 @@ while r==0:
         res=math.cos(res)
 
         print("O coseno de {} é {}!".format(num1, res))
+    #tangente
     elif desc==8:
         num1 = int(input("qual é o angulo que desega encontrar a tangente? "))
 
@@ -94,13 +103,14 @@ while r==0:
         res=math.tan(res)
 
         print("A tangente de {} é {}!".format(num1, res))
+    #caso o numero escolhido fot invalido
     else:
         print("Erro 1 - opção invalida!")
 
-    reps=1
-    t=int(input("deseja realizar outra operação? sim-0, não-1: "))#agora vai!
-    if t==1:
+    reps=1#define a variavel reps pra 1, sendo assim o menu de seleção ira aparecer
+    t=int(input("deseja realizar outra operação? sim-0, não-1: "))#escolha se continua ou não a realizar operações
+    if t==1:#se a escolha for 1(não) r vai receber 1, sendo assim ira sair do loop while
         r=r+1
 
 else:
-    print("Obrigado por utilizar a calculadora!")
+    print("Obrigado por utilizar a calculadora!")#mensagem final.
---
+++ matriz/main.py
@@ -0,0 +1,7 @@
+import math
+import numpy as np
+
+nums=list(range(5))
+
+for num in nums:
+    print(num)
